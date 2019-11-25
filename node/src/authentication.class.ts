8f1909ffb8222ddac67333388a59d7b5bb77ee7a
guanyu <yue.guanyu@hotmail.com>
2019-11-25 21:27:51 +0800

    add authentication

--- node/src/authentication.class.ts
+++ node/src/authentication.class.ts
@@ -1,16 +1,27 @@
+import rp from 'request-promise-native';
+import _ from 'lodash';
+
 export class AuthenticationRequest {
     strategy?: string;
     [key: string]: any;
 }
 
 export class Authentication {
-    constructor() {}
-    async authenticate(data: AuthenticationRequest) {
-        return {
-            accessToken: 'token',
-            authentication: {
-                strategy: 'local'
-            }
-        };
+    requsetOption: any = {
+        method: 'POST',
+        uri: 'http://127.0.0.1:3030/authentication',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        json: true
+    };
+    constructor({ protocol, host, port, method }: any = {}) {
+        this.requsetOption.uri = ``
+    }
+
+    async authenticate(body: AuthenticationRequest) {
+        let option = { body, ...this.requsetOption };
+        console.log('TCL: Authentication -> authenticate -> option', option);
+        return await rp({ body, ...this.requsetOption });
     }
 }
--- node/src/client.ts
+++ node/src/client.ts
@@ -2,10 +2,10 @@ const socket = require('socket.io-client')('http://localhost:3000', {});
 socket.on('connect', function() {
     console.log('connected');
     // auto reauthentication
-    socket.emit('authentication', {
-        strategy: 'jwt',
-        accessToken: 'test token'
-    });
+    // socket.emit('authentication', {
+    //     strategy: 'jwt',
+    //     accessToken: 'test token'
+    // });
 });
 socket.on('message', (data: any) => {
     // response message
@@ -18,12 +18,15 @@ socket.on('event', function(data: any) {
 socket.on('disconnect', function() {
     console.log('TCL: disconnect');
 });
+socket.on('authentication', (data: any) => {
+    console.log('TCL: data', data);
+});
 setTimeout(() => {
     socket.emit('event', { test: 'tset' });
     socket.send({ test: 'test' });
     socket.emit('authentication', {
         strategy: 'local',
-        email: '',
-        password: ''
+        email: '18534572861',
+        password: 'qiushanyu666'
     });
 }, 3000);
