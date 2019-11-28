b5b74143bf07220984fa7d67b3fb25bef29552f0
guanyu <yue.guanyu@hotmail.com>
2019-11-28 19:26:05 +0800

    prepare for use hooks

--- node/src/message/index.ts
+++ node/src/message/index.ts
@@ -1,5 +1,5 @@
 import { Application, CHAT_HANDLER } from 'mikudos-socketio-app';
 
 export default function(app: Application) {
-    app.chat_services = new CHAT_HANDLER(app);
+    app.chat_services = new CHAT_HANDLER(app, {});
 }
--- node/src/message/message.hooks.ts
+++ node/src/message/message.hooks.ts
@@ -2,19 +2,8 @@ import _ from 'lodash';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default {
-    before: {
-        all: [
-            (request: any) => {
-                request.params[1] -= 2;
-            }
-        ]
-    },
-
-    after: {
-        all: []
-    },
-
-    error: {
-        all: []
-    }
+    all: [(data: any, socket: SocketIO.Socket) => {}],
+    chat: [],
+    join: [],
+    leave: []
 };
