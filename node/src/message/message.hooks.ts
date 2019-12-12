b4f4ad23f69e85603538dce703ba46b7b05061d6
guanyu <yue.guanyu@hotmail.com>
2019-12-12 11:46:10 +0800

    change to newest implementation

--- node/src/message/message.hooks.ts
+++ node/src/message/message.hooks.ts
@@ -1,9 +1,16 @@
 import _ from 'lodash';
+import { mikudos } from 'mikudos-socketio-app';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default {
-    all: [(data: any, socket: SocketIO.Socket) => {}],
-    chat: [],
-    join: [],
-    leave: []
+    all: [(data: any, socket: mikudos.Socket) => {}],
+    chat: [
+        async (data: any, socket: mikudos.Socket) => {
+            console.log('TCL: socket chat');
+            let rooms = await socket.mikudos.app.clientRooms(socket);
+            console.log('rooms', rooms);
+        }
+    ],
+    join: [(data: any, socket: mikudos.Socket) => {}],
+    leave: [(data: any, socket: mikudos.Socket) => {}]
 };
